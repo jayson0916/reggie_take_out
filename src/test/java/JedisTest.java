5527bc9535d5a862585199114dfad4002bf5a53c
jaysonPeng <pengjiashun@aliyun.com>
2023-07-02 00:20:57 +0800

    test2

---
+++ src/test/java/JedisTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import redis.clients.jedis.Jedis;
+
+/**
+ * @Author: Jayson_P
+ * @Date: 2023/07/02/0:12
+ */
+public class JedisTest {
+    @Test
+    public void testRedis(){
+        //获取连接
+        Jedis jedis = new Jedis("localhost", 6379);
+        //执行操作
+        jedis.set("username","xiaoming");
+        //关闭连接
+        jedis.close();
+    }
+}
