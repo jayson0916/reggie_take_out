ff51f9d74a2136b7dacb4eb7456f77104a71c264
jaysonPeng <pengjiashun@aliyun.com>
2023-07-02 21:30:11 +0800

    sms

--- src/main/java/wut/pjs/reggie/util/SMSUtils.java
+++ src/main/java/wut/pjs/reggie/util/SMSUtils.java
@@ -27,7 +27,7 @@ public class SMSUtils {
 		java.util.List<String> args = java.util.Arrays.asList(args_);
 		// 请确保代码运行环境设置了环境变量 ALIBABA_CLOUD_ACCESS_KEY_ID 和 ALIBABA_CLOUD_ACCESS_KEY_SECRET。
 		// 工程代码泄露可能会导致 AccessKey 泄露，并威胁账号下所有资源的安全性。以下代码示例使用环境变量获取 AccessKey 的方式进行调用，仅供参考，建议使用更安全的 STS 方式，更多鉴权访问方式请参见：https://help.aliyun.com/document_detail/378657.html
-		com.aliyun.dysmsapi20170525.Client client = SMSUtils.createClient(System.getenv("LTAI5tChmBAv7ESfLKxWhd2X"), System.getenv("tL0pJBKpfd0fqc4WWu6k8GzEjthYgJ"));
+		com.aliyun.dysmsapi20170525.Client client = SMSUtils.createClient(System.getenv("1"), System.getenv("2"));
 		com.aliyun.dysmsapi20170525.models.SendSmsRequest sendSmsRequest = new com.aliyun.dysmsapi20170525.models.SendSmsRequest()
 				.setSignName("阿里云短信测试")
 				.setTemplateCode("SMS_154950909")
